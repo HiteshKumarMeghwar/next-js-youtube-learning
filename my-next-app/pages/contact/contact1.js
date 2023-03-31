2c7829f25753bfbc96f6b401860064ec9a774a81
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2023-03-31 12:49:57 +0500

    updated

---
+++ my-next-app/pages/contact/contact1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact1 = () => {
+    return (
+        <div>
+            <h1>Contact one</h1>
+        </div>
+    )
+}
+
+export default contact1
---
+++ my-next-app/pages/index.js
@@ -0,0 +1,8 @@
+const index = () => {
+  return (
+    <>
+      <h1>Index file</h1>
+    </>
+  )
+}
+export default index
